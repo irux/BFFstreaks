b50c6eb52bacc64e87ba14b8f2d40615ab3e54d8
Alejandro Jaramillo <alejojaramillo96@gmail.com>
2019-08-17 10:47:19 +0200

    layout for profile page and started working on rankings page

--- src/app/rankings/rankings.page.ts
+++ src/app/rankings/rankings.page.ts
@@ -8,5 +8,124 @@ import { Component } from '@angular/core';
 export class RankingsPage {
 
   constructor() {}
+  
+  //rankings object
+  rankings:{nearby,global} = {
+    nearby:{
+      streaks : [
+        {
+          nickname: "vicky133",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:21,
+          hours_left:20,
+          last_met:1
+        },
+        {
+          nickname: "alejo96",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:12,
+          hours_left:12,
+          last_met:1
+        },
+        {
+          nickname: "beatrice",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:9,
+          hours_left:3,
+          last_met:1
+        }
+      ],
+      all : [
+        {
+          nickname: "valentino",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:1,
+          total_count:30
+        },
+        {
+          nickname: "alejo96",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:10,
+          total_count:10
+        },
+        {
+          nickname: "beatriice11",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:3,
+          total_count:8
+        }
+      ]
+    },
+    global : {
+      streaks : [
+        {
+          nickname: "vicky133",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:121,
+          hours_left:20,
+          last_met:1
+        },
+        {
+          nickname: "alejo96",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:112,
+          hours_left:12,
+          last_met:1
+        },
+        {
+          nickname: "beatrice",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          streak:true,
+          streak_count:19,
+          hours_left:3,
+          last_met:1
+        }
+      ],
+      all : [
+        {
+          nickname: "valentino",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:1,
+          total_count:30
+        },
+        {
+          nickname: "alejo96",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:10,
+          total_count:10
+        },
+        {
+          nickname: "beatriice11",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:3,
+          total_count:8
+        },
+        {
+          nickname: "mamma",
+          avatar:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw==",
+          longest_streak:1,
+          total_count:2
+        }
+      ]
+    }
+  }
+  
+  //Change the list that you're looking at
+  view_list:String = "nearby" //default
+  active_list:any = this.rankings.nearby //default to nearby
+  selectList(name:String){
+    this.view_list = name
+    if (this.view_list == "nearby") {
+      this.active_list = this.rankings.nearby
+    } else {
+      this.active_list = this.rankings.global
+    }
+  }
+
 
 }
